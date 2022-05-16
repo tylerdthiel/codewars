f0e93e84fd2066e4dc21ad263d12b17f1c0ad9fa
Tyler Thiel <tylerdthiel@gmail.com>
2022-05-15 21:41:50 -0600

    reworked this problem. found a better solution

--- js/dontGiveMeFive.js
+++ js/dontGiveMeFive.js
@@ -1,10 +1,13 @@
 function dontGiveMeFive(start, end){
     let result = 0
     for (let i = start; i <= end; i++){
-      console.log(i)
       if (!i.toString().includes(5)){
         result++
       }
     }
     return result
   }
+
+  console.log(dontGiveMeFive(1, 100))
+
+  console.log(dontGiveMeFive(5, 20))
